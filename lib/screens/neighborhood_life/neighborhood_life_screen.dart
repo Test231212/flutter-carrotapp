fab7135eac1a404f2d479ff47b45f7f9b06a79ad
test231212 <j910219@gmail.com>
2024-04-18 15:26:12 +0900

    진행중

--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,10 +1,35 @@
+import 'package:carrot_app/models/neighborhood_life.dart';
+import 'package:carrot_app/screens/components/appbar_preferred_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'components/life_header.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('neighborhoodLifeScreen'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('동네생활'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle)),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell))
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            // child: LifeBody(neighborhoodLife: neighborhoodLifeList[0]),
+          )
+        ],
+      ),
     );
   }
 }
